c05c3095425a75dfa2ca84d0272dbfd575eacfee
Eric <ericmansur13@gmail.com>
2026-01-05 18:17:31 -0300

    correções

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "bootstrap"
-import "../stylesheets/application"
+
+// Rails UJS precisa ser carregado assim
+import Rails from "@rails/ujs";
+Rails.start();
