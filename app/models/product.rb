40bb809f6c99ebee7f24feb91d1b6d4dcece2394
Eric <ericmansur13@gmail.com>
2026-01-05 19:11:09 -0300

    Crud atualizado com mensagens

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
-  validates :name, presence: true
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
